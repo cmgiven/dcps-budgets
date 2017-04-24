010100769d45fe07c7600f3b558f01dad9987c48
Chris Given <cmgiven@gmail.com>
2017-04-23 20:02:32 -0400

    Build (and another Gulp fix)

--- gulpfile.js
+++ gulpfile.js
@@ -15,14 +15,10 @@ gulp.task('styles', function () {
 });
 
 gulp.task('html', ['styles'], function () {
-  var assets = $.useref.assets({searchPath: '{.tmp,app}'});
-
   return gulp.src('app/*.html')
-    .pipe(assets)
+    .pipe($.useref({searchPath: '{.tmp,app}'}))
     .pipe($.if('*.js', $.uglify()))
     .pipe($.if('*.css', $.csso()))
-    .pipe(assets.restore())
-    .pipe($.useref())
     .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
     .pipe(gulp.dest('dist'));
 });
